60355ed7daded650f96c292dda78ff208af3ed89
Andrae Vang <andrae.vang@gmail.com>
2024-10-02 09:15:58 -0700

    completed exercise 1 Loop

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,76 @@
+﻿//Exercise 1
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; int++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
